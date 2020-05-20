05653d787fd41ca5457076620deff1b82c07a8b1
yas <left2right2015@gmail.com>
2020-05-20 21:38:28 +0900

    stripe apiから商品情報を取得

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,16 +1,17 @@
-import React from "react"
-import Button from "@material-ui/core/Button"
-import Card from "@material-ui/core/Card"
-import CardActions from "@material-ui/core/CardActions"
-import CardContent from "@material-ui/core/CardContent"
-import CardMedia from "@material-ui/core/CardMedia"
-import Grid from "@material-ui/core/Grid"
-import Typography from "@material-ui/core/Typography"
-import { makeStyles } from "@material-ui/core/styles"
-import Container from "@material-ui/core/Container"
+import React, { useState, useEffect } from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   heroContent: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6),
@@ -28,27 +29,33 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "column",
   },
   cardMedia: {
-    paddingTop: "56.25%", // 16:9
+    paddingTop: "100%",
   },
   cardContent: {
     flexGrow: 1,
   },
-}))
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+}));
 
 const ItemList = () => {
-  const classes = useStyles()
+  const classes = useStyles();
   const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+  const [products, setProducts] = useState({ data: [] });
+  const [prices, setPrices] = useState({ data: [] });
 
   const buyClick = async (event) => {
+    const productId = event.currentTarget.dataset.productId;
+    const price = prices.data.find((price) => price.product === productId);
+    if (price == null) {
+      return;
+    }
+
     // When the customer clicks on the button, redirect them to Checkout.
     const DOMAIN = window.location.href.replace(/[^/]*$/, "");
     const stripe = await stripePromise;
     const { error } = await stripe.redirectToCheckout({
       lineItems: [
         // Replace with the ID of your price
-        { price: "price_HJ59tJJ3IO3n0g", quantity: 1 },
+        { price: price.id, quantity: 1 },
       ],
       mode: "payment",
       successUrl: `${DOMAIN}success`,
@@ -61,6 +68,40 @@ const ItemList = () => {
     // If `redirectToCheckout` fails due to a browser or network
     // error, display the localized error message to your customer
     // using `error.message`.
+  };
+
+  useEffect(() => {
+    const username = process.env.REACT_APP_STRIPE_SERCRET_KEY;
+    async function fetchData() {
+      try {
+        const resProducts = await axios.get(
+          "https://api.stripe.com/v1/products",
+          {
+            headers: {
+              Authorization: `Bearer ${username}`,
+            },
+          }
+        );
+        const resPrices = await axios.get("https://api.stripe.com/v1/prices", {
+          headers: {
+            Authorization: `Bearer ${username}`,
+          },
+        });
+        setProducts(resProducts.data);
+        setPrices(resPrices.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const getPrice = (id) => {
+    const price = prices.data.find((price) => price.product === id);
+    if (price == null) {
+      return;
+    }
+    return price.unit_amount;
   };
 
   return (
@@ -89,22 +130,30 @@ const ItemList = () => {
       <Container className={classes.cardGrid} maxWidth="md">
         {/* End hero unit */}
         <Grid container spacing={4}>
-          {cards.map((card) => (
-            <Grid item key={card} xs={6} sm={6} md={4}>
+          {products.data.map((item) => (
+            <Grid item key={item.id} xs={6} sm={6} md={4}>
               <Card className={classes.card}>
                 <CardMedia
                   className={classes.cardMedia}
-                  image="https://source.unsplash.com/random"
+                  image={item.images[0]}
                   title="Image title"
                 />
                 <CardContent className={classes.cardContent}>
                   <Typography gutterBottom variant="h5" component="h2">
-                    商品名
+                    {item.name}
                   </Typography>
-                  <Typography>商品詳細文言</Typography>
+                  <Typography>{item.description}</Typography>
+                  <Typography gutterBottom variant="h6" component="h2">
+                    ¥{getPrice(item.id)}
+                  </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small" color="primary" onClick={buyClick}>
+                  <Button
+                    size="small"
+                    color="primary"
+                    data-product-id={item.id}
+                    onClick={buyClick}
+                  >
                     今すぐ購入
                   </Button>
                 </CardActions>
@@ -115,5 +164,5 @@ const ItemList = () => {
       </Container>
     </div>
   );
-}
+};
 export default ItemList;
